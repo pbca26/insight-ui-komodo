016a82805939e90022a407d69dcd8ac3a9489c25
pbca26 <pbca26@gmail.com>
2019-05-14 11:47:59 +0300

    url params parser handle extra slash in coin name

--- public/js/url.params.js
+++ public/js/url.params.js
@@ -3,6 +3,6 @@ function getCoinUrlParam(url) {
 
   if (params &&
       params[1]) {
-    return params[1];
+    return params[1].indexOf('/') > -1 ? params[1].replace('/', '') : params[1];
   }
 }
